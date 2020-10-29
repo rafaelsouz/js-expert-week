0b7043d69c17d9da737d5637f9ce64d777c2e446
Rafael Souza <rafael.souza@umbu.tech>
2020-10-29 20:47:25 +0000

    preparando os arquivos para exibição no player.

--- script.sh
+++ script.sh
@@ -1,5 +1,4 @@
 ASSETSFOLDER=assets/timeline
-
 for mediaFile in `ls $ASSETSFOLDER | grep .mp4`; do
     # cortar a extensão e a resolução do arquivo
     FILENAME=$(echo $mediaFile | sed -n 's/.mp4//p' | sed -n 's/-1920x1080//p')
